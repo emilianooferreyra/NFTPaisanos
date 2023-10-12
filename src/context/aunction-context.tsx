e2b69fb5f95dfdc6cc3ab670fd0a36568ca468f9
emilianooferreyra <emilianooferreyra@gmail.com>
2023-10-12 10:17:52 -0300

    add aunctioncontextype in context

--- src/context/aunction-context.tsx
+++ src/context/aunction-context.tsx
@@ -3,12 +3,19 @@ import React, { createContext, useContext } from 'react';
 import { aunctions } from '../data/aunctions';
 import { popularAunctions } from '../data/popular-aunctions';
 import { ethUsd } from '../data/eth-usd';
+import { aunctionsData } from '../types/AunctionsData';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const AunctionContext = createContext<any | null>(null);
+interface AunctionContextType {
+  aunctions: aunctionsData[] | any;
+  popularAunctions: aunctionsData[] | any;
+  ethUsd: aunctionsData | any;
+}
+
+const AunctionContext = createContext<AunctionContextType | null>(null);
 
 export const AunctionProvider = ({ children }: Props) => (
   <AunctionContext.Provider value={{ aunctions, popularAunctions, ethUsd }}>{children}</AunctionContext.Provider>
